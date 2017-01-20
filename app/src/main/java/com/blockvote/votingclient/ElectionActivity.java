c349d3776d136b10bc64e6dd40e3f430ac681dbb
AndreiBuiza <andreibuiza@gmail.com>
2017-01-20 12:58:32 -0700

    State management code is halfway done in Election Activity, added all the fragments to be used by ElectionActivity

--- app/src/main/java/com/blockvote/votingclient/ElectionActivity.java
+++ app/src/main/java/com/blockvote/votingclient/ElectionActivity.java
@@ -1,6 +1,7 @@
 package com.blockvote.votingclient;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
@@ -17,6 +18,7 @@ public class ElectionActivity extends AppCompatActivity
         SelectCandidateFragment.OnFragmentInteractionListener,
         BallotConfirmationFragment.OnFragmentInteractionListener{
     private final String LOG_TAG = ElectionActivity.class.getSimpleName();
+    private String stateKey = getString(R.string.ElectionActivityState);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +28,8 @@ public class ElectionActivity extends AppCompatActivity
         //get the extra data from intent
         Intent intent = getIntent();
 
-        //TODO: use the electionName to access the DB later on
-        //TODO: only the ElectionActivity will have access to the DB
         String electionName=intent.getStringExtra(getString(R.string.selectedElectionKey));
-
+        Log.d(LOG_TAG, electionName + " selected");
         //Toolbar setuo
         Toolbar myToolbar = (Toolbar) findViewById(R.id.electionmain_toolbar);
         setSupportActionBar(myToolbar);
@@ -38,24 +38,57 @@ public class ElectionActivity extends AppCompatActivity
         myActionBar.setDisplayHomeAsUpEnabled(true);
         myActionBar.setTitle(electionName);
 
-        //TODO:select the appropriate fragment to display according to the data from the DB
+        //TODO: add a developer only reset button to reset the state. (This is for testing purposes only)
 
-        if (savedInstanceState == null) {
-
-            //TODO: Call the VoteButtonFragment (for testing purposes only)
-            VoteButtonFragment voteButtonFragment = new VoteButtonFragment();
-            getSupportFragmentManager().beginTransaction().add(R.id.ElectionContainer, voteButtonFragment).commit();
-
-        }else{
-            Log.d(LOG_TAG, "savedInstanceState is not null");
+        //TODO:select the appropriate fragment to display according to the data from the DB, (for now I will use the simple SharedPreferences)
+        SharedPreferences dataStore = getPreferences(MODE_PRIVATE);
+        stateKey += electionName;
+        if (!dataStore.contains(stateKey)){
+            //Init the state
+            SharedPreferences.Editor editor = dataStore.edit();
+            editor.putString(stateKey, getString(R.string.RegistrationState));
+        }
+        //get the current state
+        String currentState =dataStore.getString(stateKey, "error");
+        if(currentState.equals("error")){
+            Log.e(LOG_TAG, "Could not find the current state of ElectionActivity");
+            throw new RuntimeException("State missing... ");
         }
 
-        Log.d(LOG_TAG, electionName + " selected");
+        if (savedInstanceState == null) {
+            if(currentState.equals(R.string.RegistrationState)){
+                RegisterFragment registerFragment = new RegisterFragment();
+                getSupportFragmentManager().beginTransaction().add(R.id.ElectionContainer, registerFragment).commit();
+            }
+            if(currentState.equals(R.string.RegistrationStatusState)){
+                RegistrationStatusFragment registrationStatusFragment = new RegistrationStatusFragment();
+                getSupportFragmentManager().beginTransaction().add(R.id.ElectionContainer, registrationStatusFragment).commit();
+            }
+            if(currentState.equals(R.string.VoteButtonState)){
+                VoteButtonFragment voteButtonFragment = new VoteButtonFragment();
+                getSupportFragmentManager().beginTransaction().add(R.id.ElectionContainer, voteButtonFragment).commit();
+            }
+            if(currentState.equals(R.string.ReviewBallotState)){
+                ReviewBallotFragment reviewBallotFragment = new ReviewBallotFragment();
+                getSupportFragmentManager().beginTransaction().add(R.id.ElectionContainer, reviewBallotFragment).commit();
+            }
+            Log.e(LOG_TAG, "Could not start the appropriate fragment");
+            throw new RuntimeException("fragment missing... ");
+
+
+        }else{
+            //TODO: protect the activity and its current fragments if it is somehow destroyed
+            Log.e(LOG_TAG, "The Election Activity was temporarily destroyed, and now it has nowhere to go...");
+            throw new RuntimeException("Unexpected destruction of Election Activity");
+        }
+
+
 
 
     }
 
     /**
+     * VoteButtonFragment will be switched to the SelectcandidateFragment
      * VoteButtonFragment will call this function
      */
     public void onVoteButtonInteraction(){
@@ -71,6 +104,7 @@ public class ElectionActivity extends AppCompatActivity
     }
 
     /**
+     * SelectCandidateFragment will be switched with BallotConfirmationFragment
      * SelectcandidateFragment will call this function
      * @param firstName
      * @param lastName
