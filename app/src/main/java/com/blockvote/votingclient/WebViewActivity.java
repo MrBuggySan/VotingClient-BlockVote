b630a98e0fe9cf0c7135e2dc0f3742ee679d5c5d
mrbuggysan <andreibuiza@gmail.com>
2017-03-25 09:50:55 -0600

    fixed conflict

--- app/src/main/java/com/blockvote/votingclient/WebViewActivity.java
+++ app/src/main/java/com/blockvote/votingclient/WebViewActivity.java
@@ -1,11 +1,16 @@
 package com.blockvote.votingclient;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.webkit.WebView;
 
-public class WebViewActivity extends AppCompatActivity {
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP;
+import static android.content.Intent.FLAG_ACTIVITY_NO_ANIMATION;
 
+public class WebViewActivity extends AppCompatActivity {
+    private String LOG_TAG = WebViewActivity.class.getSimpleName();
     private WebView webView;
 
     @Override
@@ -16,5 +21,15 @@ public class WebViewActivity extends AppCompatActivity {
         webView = (WebView) findViewById(R.id.webView1);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://conzimmer.github.io/resultsPage.html");
+    }
+
+    @Override
+    public void onBackPressed(){
+        Log.d(LOG_TAG, "Back pressed");
+        //Call the MainActivity
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(FLAG_ACTIVITY_NO_ANIMATION);
+        intent.setFlags(FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
